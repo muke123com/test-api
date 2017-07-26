49f54aa763e4f7d75aa558d68c5926037e04094a
mukeke <mukeke@quandashi.com>
2017-07-26 18:28:50 +0800

    载入模型

--- server/public/js/webgl.js
+++ server/public/js/webgl.js
@@ -1,11 +1,26 @@
+/**
+ * three 基础
+ */
+var baseLoaded = 0;
+var objLoaded = 0;
+
+function $(el){
+	return document.querySelector(el);
+}
+
 function baseThree(){
+	$(".webgl-content").style.display = "none";
+	$(".webgl-base").style.display = "block";
+	if(baseLoaded == 1){
+		return false;
+	}
 	var scene = new THREE.Scene();
 	//1.视野角， 2.纵横比， 3.最近距离， 4.最远距离
 	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);  
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMap.enabled = true;  
-	document.querySelector(".webgl-content").appendChild( renderer.domElement );
+	$(".webgl-base").appendChild(renderer.domElement);
 	//载入背景贴图	
 	//http://www.humus.name/index.php?page=Textures 图片下载地址
 	var cube_loader = new THREE.CubeTextureLoader();
@@ -25,7 +40,7 @@ function baseThree(){
 		renderer.render(scene, camera);
 	})
 	//cube
-	var texture = THREE.ImageUtils.loadTexture('/images/crate.gif');  //载入贴图
+	var texture = THREE.TextureLoader('/images/crate.gif');  //载入贴图
 	var geometry = new THREE.BoxGeometry(10,10,10);
 	var cube_material = new THREE.MeshLambertMaterial( { color: 0xf08b2f } ); 
 	cube_material.map = texture;  //材质添加贴图
@@ -87,4 +102,60 @@ function baseThree(){
 		renderer.render( scene, camera );
 	}
 	animate();
+	baseLoaded = 1;
+}
+
+
+/**
+ * 载入obj模型
+ */
+function loadObj(){
+	$(".webgl-content").style.display = "none";
+	$(".webgl-obj").style.display = "block";
+	if(objLoaded == 1){
+		return false;
+	}
+	//初始化场景
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);  
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMap.enabled = true;  
+	$(".webgl-obj").appendChild(renderer.domElement);
+	//控制
+	var controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.addEventListener('change', function() {
+		renderer.render(scene, camera);
+	})
+	//光源
+	var light = new THREE.AmbientLight(0xcccccc);  
+	scene.add(light);  
+	//贴图载入
+	var texture = new THREE.Texture();
+	var t_loader = new THREE.ImageLoader();
+	t_loader.load('/obj/cerberus/Cerberus_A.jpg', function(image){
+		texture.image = image;
+		texture.needsUpdate = true;
+	})
+	//模型载入
+	var obj_loader = new THREE.OBJLoader();
+	obj_loader.load(
+		'/obj/cerberus/Cerberus.obj',
+		function (obj) {
+			obj.traverse(function(child){
+				if ( child instanceof THREE.Mesh ) {	
+					child.material.map = texture;
+				}
+			})
+			scene.add( obj );
+		}
+	)
+	camera.position.set(0, 10, 20);
+	function animate() {
+		requestAnimationFrame( animate );
+		renderer.render( scene, camera );
+	}
+	animate();
+	
+	objLoaded = 1;
 }
